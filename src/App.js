7c8c0d5ca72d7e7b22b68322de94e7375cbf293e
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2020-07-05 09:39:37 -0400

    usedispatch, action, payload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions/index'
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const Logged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch()
   return (
     <div className="App">
-      hey
+      <h1>Counter {counter}</h1>
+      <button onClick={() => (dispatch(increment(5)))}> + </button>
+      <button onClick={() => dispatch(decrement(2))}> - </button>
+
+    <h3>Is user Logged? {Logged ? 'yes' : 'no'}</h3>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
-import allReducer from './reducers/index';
+import allReducers from './reducers/index';
+import {Provider} from 'react-redux';
 
+const store = createStore(
+  allReducers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
-
-
+  
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
